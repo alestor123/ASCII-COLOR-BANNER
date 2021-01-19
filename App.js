cfc505b246af380363c68b0dd86af677e6607123
alestor123 <alestoraldous@gmail.com>
2021-01-19 13:56:11 +0530

    added main code

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 var axios = require('axios')
 module.exports = async name => {
 if(!name) throw Error('Name not found')
-
+var data = await axios.get(`https://api.npmjs.org/downloads/point/last-year/${name}`) 
+return data.data.downloads>5000
 }
