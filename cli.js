ca3fadf9c1f4ae7ddf3d33cec307406621821161
alestor123 <alestoraldous@gmail.com>
2021-01-19 14:00:32 +0530

    added cli

--- cli.js
+++ cli.js
@@ -1,1 +1,7 @@
 #!/usr/bin/env node
+var ispopular = require('./App');
+(async () => {
+var data = await ispopular(process.argv[2])
+if(data) console.log('Yeah its pretty popular')
+else console.log('Still Below 5000')
+})();
